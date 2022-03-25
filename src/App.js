cf283e976c4d1751be0e6bb7bc25f133896fdabb
AzmiHabib <azmihabib0323@gmail.com>
2022-03-25 21:27:23 +0600

    edit redme file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Header></Header>
     </div>
   );
 }
